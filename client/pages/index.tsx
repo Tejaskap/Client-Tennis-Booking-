4095fed3cccb92e0c99762a3c3f03b419017dbe4
Tejas <tejaskapoor97x@gmail.com>
2023-12-04 18:59:49 +0100

    Latest

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,18 +1,42 @@
 import React, { useEffect, useState } from "react";
 
-function index() {
+function Index() {
   const [message, setMessage] = useState("");
+  const [timeSlots, setTimeSlots] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8080/api/home").then((response) => {
-      response.json().then((data) => {
+    fetch("http://localhost:8080/api/home")
+      .then((response) => response.json())
+      .then((data) => {
         console.log(data);
         setMessage(data.message);
       });
-    });
+
+    const generateTimeSlots = () => {
+      const startTime = 7;
+      const endTime = 21;
+      const slots = [];
+      for (let i = startTime; i <= endTime; i++) {
+        slots.push(`${i.toString().padStart(2, "0")}:00`);
+      }
+      setTimeSlots(slots);
+    };
+
+    generateTimeSlots();
   }, []);
 
-  return <div>{message}</div>;
+  return (
+    <div className="container">
+      <h1>{message}</h1>
+      <div className="time-slots-container">
+        {timeSlots.map((timeSlot, index) => (
+          <div key={index} className="time-slot">
+            {timeSlot}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default index;
+export default Index;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
 // const cors = require("cors");
-const PORT = "8080";
+const PORT = "5001";
 const routes = require("./routes");
 
 // Registering routes
-// app.use(routes);
+app.use(routes);
 
-// app.get("/api/home", (req, res) => {
-//   res.json({ message: "Hello, world" });
-// });
+app.get("/api/home", (req, res) => {
+  res.json({ message: "Hello, world" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
