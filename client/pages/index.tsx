17e5f4e041e188130e7d3bda979cc0f525bcbaee
Tejas <tejaskapoor97x@gmail.com>
2023-12-18 10:18:08 +0100

    cleaned

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,41 +2,16 @@ import React, { useEffect, useState } from "react";
 
 function Index() {
   const [message, setMessage] = useState("");
-  const [timeSlots, setTimeSlots] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8080/api/home")
+    // Fetch data from the server
+    fetch("http://localhost:5001/api/home")
       .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setMessage(data.message);
-      });
-
-    const generateTimeSlots = () => {
-      const startTime = 7;
-      const endTime = 21;
-      const slots = [];
-      for (let i = startTime; i <= endTime; i++) {
-        slots.push(`${i.toString().padStart(2, "0")}:00`);
-      }
-      setTimeSlots(slots);
-    };
-
-    generateTimeSlots();
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
-  return (
-    <div className="container">
-      <h1>{message}</h1>
-      <div className="time-slots-container">
-        {timeSlots.map((timeSlot, index) => (
-          <div key={index} className="time-slot">
-            {timeSlot}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  return <div>{message}</div>;
 }
 
 export default Index;
